0d6fde8eaee74092af840d162a59c4df5e23cd6e
Lucas_Barth <lucasbarth@ienh.com.br>
2021-04-02 10:48:55 -0300

    Animação Jogo

---
+++ Assets/Scenes/Personagens/BackgroundScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    private float screenWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+    
+        float imageWidth = renderer.sprite.bounds.size.x;
+        float imageHeight = renderer.sprite.bounds.size.x;
+
+        float screenHeight = Camera.main.orthographicSize * 4;
+        screenWidth = screenHeight / Screen.height * Screen.width;
+
+        Vector2 newScale = this.transform.localScale;
+        newScale.x = screenWidth / imageWidth + 1f;
+        newScale.y = screenHeight / imageHeight;
+        this.transform.localScale = newScale;
+
+        body.velocity = new Vector2(-5,0);
+
+         if (this.name == "Background2"){
+            this.transform.position = new Vector2(screenWidth, 0);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.x <= -screenWidth){
+            this.transform.position = new Vector2(screenWidth, 0);
+        }
+    }
+}
---
+++ Assets/Scenes/Personagens/Felipino/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    SpriteRenderer sr;
+
+    Rigidbody2D rb;
+    public float forca = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.position += new Vector3(6 * speed * Time.deltaTime, 0, 0);
+            sr.flipX = false;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.position -= new Vector3(6 * speed * Time.deltaTime, 0, 0);
+            sr.flipX = true;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            rb.AddForce(transform.up* forca);
+        }
+    }
+}
